c0dcad3b98eb39a09bdec85a5496543d1525b6c4
Joe <36609959+jmushailov@users.noreply.github.com>
2020-07-21 11:04:56 -0400

    Create future.py
    Predicting the future

---
+++ future.py
@@ -0,0 +1,21 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#              Predicting the future
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from numpy import newaxis
+# We sorted descending
+curr_frame = X_test[len(X_test)-1]
+future = []
+
+# Quick plot of the frame we're predicting from
+plt.plot(curr_frame)
+
+points_to_predict = 10
+for i in range(points_to_predict):
+      # append the prediction to our empty future list
+     future.append(model.predict(curr_frame[newaxis,:,:])[0,0])
+      # insert our predicted point to our current frame
+     curr_frame = np.insert(curr_frame, len(X_test[0]), future[-1], axis=0)
+      # push the frame up one to make it progress into the future
+     curr_frame = curr_frame[1:]
+     
+ plt.plot(future)    
